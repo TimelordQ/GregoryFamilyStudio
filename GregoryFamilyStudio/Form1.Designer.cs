49a6ea063d04c9cddd39da9055138b1deb3f2419
Q <universalbri@gmail.com>
2024-10-03 21:58:53 -0700

    Add files via upload

---
+++ GregoryFamilyStudio/Form1.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace GregoryFamilyStudio
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.picResizer = new System.Windows.Forms.PictureBox();
+            this.lblSize = new System.Windows.Forms.Label();
+            this.lvMain = new System.Windows.Forms.ListView();
+            this.mediaName = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.runTime = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.lblCurRunTime = new System.Windows.Forms.Label();
+            this.tmrShowRuntime = new System.Windows.Forms.Timer(this.components);
+            this.tmrEndReached = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.picResizer)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picResizer
+            // 
+            this.picResizer.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.picResizer.BackColor = System.Drawing.Color.Transparent;
+            this.picResizer.Location = new System.Drawing.Point(31, 18);
+            this.picResizer.Name = "picResizer";
+            this.picResizer.Size = new System.Drawing.Size(1218, 684);
+            this.picResizer.TabIndex = 0;
+            this.picResizer.TabStop = false;
+            // 
+            // lblSize
+            // 
+            this.lblSize.AutoSize = true;
+            this.lblSize.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(25)))), ((int)(((byte)(42)))));
+            this.lblSize.Font = new System.Drawing.Font("Microsoft Sans Serif", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSize.ForeColor = System.Drawing.Color.Yellow;
+            this.lblSize.Location = new System.Drawing.Point(46, 28);
+            this.lblSize.Name = "lblSize";
+            this.lblSize.Size = new System.Drawing.Size(364, 37);
+            this.lblSize.TabIndex = 1;
+            this.lblSize.Text = "Gregory Family Library";
+            // 
+            // lvMain
+            // 
+            this.lvMain.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lvMain.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(35)))), ((int)(((byte)(45)))), ((int)(((byte)(62)))));
+            this.lvMain.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.lvMain.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.mediaName,
+            this.runTime});
+            this.lvMain.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lvMain.ForeColor = System.Drawing.Color.White;
+            this.lvMain.FullRowSelect = true;
+            this.lvMain.HideSelection = false;
+            this.lvMain.Location = new System.Drawing.Point(47, 76);
+            this.lvMain.Name = "lvMain";
+            this.lvMain.Size = new System.Drawing.Size(1184, 601);
+            this.lvMain.TabIndex = 2;
+            this.lvMain.UseCompatibleStateImageBehavior = false;
+            this.lvMain.View = System.Windows.Forms.View.Details;
+            // 
+            // mediaName
+            // 
+            this.mediaName.Text = "Name";
+            this.mediaName.Width = 980;
+            // 
+            // runTime
+            // 
+            this.runTime.Text = "Run Time";
+            this.runTime.Width = 150;
+            // 
+            // lblCurRunTime
+            // 
+            this.lblCurRunTime.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblCurRunTime.AutoSize = true;
+            this.lblCurRunTime.BackColor = System.Drawing.Color.Transparent;
+            this.lblCurRunTime.Font = new System.Drawing.Font("Courier New", 24F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCurRunTime.ForeColor = System.Drawing.Color.Yellow;
+            this.lblCurRunTime.Location = new System.Drawing.Point(798, 641);
+            this.lblCurRunTime.Name = "lblCurRunTime";
+            this.lblCurRunTime.Size = new System.Drawing.Size(433, 36);
+            this.lblCurRunTime.TabIndex = 3;
+            this.lblCurRunTime.Text = "Gregory Family Library";
+            this.lblCurRunTime.Visible = false;
+            // 
+            // tmrShowRuntime
+            // 
+            this.tmrShowRuntime.Tick += new System.EventHandler(this.tmrShowRuntime_Tick);
+            // 
+            // tmrEndReached
+            // 
+            this.tmrEndReached.Interval = 250;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(15)))), ((int)(((byte)(25)))), ((int)(((byte)(42)))));
+            this.ClientSize = new System.Drawing.Size(1280, 720);
+            this.ControlBox = false;
+            this.Controls.Add(this.lblCurRunTime);
+            this.Controls.Add(this.lvMain);
+            this.Controls.Add(this.lblSize);
+            this.Controls.Add(this.picResizer);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Form1_KeyPress);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.picResizer)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picResizer;
+        private System.Windows.Forms.Label lblSize;
+        private System.Windows.Forms.ListView lvMain;
+        private System.Windows.Forms.ColumnHeader mediaName;
+        private System.Windows.Forms.ColumnHeader runTime;
+        private System.Windows.Forms.Label lblCurRunTime;
+        private System.Windows.Forms.Timer tmrShowRuntime;
+        private System.Windows.Forms.Timer tmrEndReached;
+    }
+}
+
---
+++ GregoryFamilyStudio/Form1.cs
@@ -0,0 +1,300 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using Vlc.DotNet.Forms;
+using System.Drawing.Text;
+using System.Diagnostics.Eventing.Reader;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.TaskbarClock;
+using System.Globalization;
+using System.Reflection.Emit;
+using Microsoft.Win32;
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+namespace GregoryFamilyStudio
+{
+    public partial class Form1 : Form
+    {
+        private string startDirectory;
+        private const string PATHCONSTANT = "-";
+        private string sCurDir = "";
+        private String vlcInstallDirectory;
+        private VlcControl vlcControl1;
+        private enum MovieState { Stopped = 0, Stop = 1, Play = 2, Playing = 3, Pause = 4, Paused = 5, End = 6 };
+        private MovieState curMovieState;
+        private string sCurRunTime;
+        private bool m_bShowRunTime = false;
+
+        public Form1()
+        {
+            vlcInstallDirectory = GetPathForExe("VLC.EXE");
+            vlcInstallDirectory = vlcInstallDirectory.Substring(0, vlcInstallDirectory.LastIndexOf("\\"));
+            /*
+            int nExeNameEnd = Application.ExecutablePath.LastIndexOf('.');
+            string sININame = Application.ExecutablePath.Substring(0, Application.ExecutablePath.Length - (Application.ExecutablePath.Length - nExeNameEnd )) + ".ini";
+            var MyIni = new IniFile(@sININame);
+            if (!MyIni.KeyExists("StartDirectory", "Main"))
+                startDirectory = MyIni.Read("StartDirectory", "Main");
+            */
+            if (System.Environment.MachineName == "GREGORY-PC")
+            {
+                startDirectory = "c:\\videos";
+            }
+            else
+            {
+                startDirectory = "e:\\videos";
+            }
+
+            vlcControl1 = new VlcControl();
+            InitializeComponent();
+
+            sCurDir = startDirectory;
+            refreshFiles();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            vlcControl1.BeginInit();
+            var libDirectory = new DirectoryInfo(vlcInstallDirectory);
+            vlcControl1 = new Vlc.DotNet.Forms.VlcControl();
+            vlcControl1.VlcLibDirectory = libDirectory;
+            //vlcControl1.EndReached += new System.EventHandler( _OnEndReached );
+            vlcControl1.EndReached += VlcControl1_EndReached;
+            vlcControl1.EndInit();
+            this.Controls.Add(vlcControl1); //Add the control to your container
+            vlcControl1.Dock = DockStyle.Fill; //Optional
+
+            this.Top = 0;
+            //picResizer.Height = 684; // Screen.PrimaryScreen.WorkingArea.Height;
+            //picResizer.Top = ((Screen.PrimaryScreen.WorkingArea.Height - picResizer.Height) / 2);
+            this.Left = 0;
+            //picResizer.Width = 1218; // Screen.PrimaryScreen.WorkingArea.Width;
+            //picResizer.Left = ((Screen.PrimaryScreen.WorkingArea.Width - picResizer.Width) / 2);
+            
+        }
+
+        private void VlcControl1_EndReached(object sender, Vlc.DotNet.Core.VlcMediaPlayerEndReachedEventArgs e)
+        {
+            Invoke(new Action(() =>
+            {
+                setMovieState(MovieState.End);
+            }));
+        }
+
+        private void Form1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (e.KeyChar == Convert.ToChar(Keys.Escape))
+            {
+                if (curMovieState == MovieState.Playing)
+                    setMovieState(MovieState.Stop);
+                else
+                    Application.Exit();
+            }
+
+            if (e.KeyChar == Convert.ToChar(Keys.Space) && 
+                ( ( curMovieState == MovieState.Playing ) || curMovieState == MovieState.Paused ) )
+            {
+                if (curMovieState == MovieState.Playing)
+                    setMovieState(MovieState.Pause);
+                else
+                    setMovieState(MovieState.Play);
+            }
+            else if (e.KeyChar == Convert.ToChar(Keys.Enter))
+            {
+                if (lvMain.SelectedItems[0].Tag != null && lvMain.SelectedItems[0].Tag.ToString() == PATHCONSTANT)
+                {
+                    if (lvMain.SelectedItems[0].SubItems[0].Text == "..")
+                        sCurDir = sCurDir.Substring(0, sCurDir.LastIndexOf('\\'));
+                    else
+                        sCurDir = sCurDir + '\\' + lvMain.SelectedItems[0].SubItems[0].Text;
+
+                    refreshFiles();
+                }
+                else
+                {
+                    String sCurFile = sCurDir + '\\' + lvMain.SelectedItems[0].SubItems[0].Text;
+                    vlcControl1.SetMedia(new FileInfo(sCurFile));
+                    sCurRunTime = lvMain.SelectedItems[0].SubItems[1].Text;
+                    setMovieState(MovieState.Play);
+                }
+            }
+        }
+
+        private void setMovieState(MovieState movieState)
+        {
+            if (movieState == MovieState.Play && curMovieState == MovieState.Paused)
+            {
+                showRunTime(false);
+                vlcControl1.Play();
+            }
+            if (movieState == MovieState.Play)
+            {
+                curMovieState = MovieState.Playing;
+                picResizer.Visible = lblSize.Visible = lvMain.Visible = false;
+                vlcControl1.Play();
+            }
+            else if (movieState == MovieState.End )
+            {
+                curMovieState = MovieState.Stopped;
+                picResizer.Visible = lblSize.Visible = lvMain.Visible = true;
+                lvMain.Focus();
+            }
+            else if (movieState == MovieState.Stop)
+            {
+                vlcControl1.Stop();
+                curMovieState = MovieState.Stopped;
+                picResizer.Visible = lblSize.Visible = lvMain.Visible = true;
+                lvMain.Focus();
+            }
+            else if (movieState == MovieState.Pause)
+            {
+                showRunTime(true);
+                curMovieState = MovieState.Paused;
+                vlcControl1.Pause();
+            }
+        }
+
+        private void showRunTime(bool v)
+        {
+            if (v)
+            {
+                DateTime totalTime = DateTime.ParseExact(sCurRunTime, "HH:mm:ss", CultureInfo.InvariantCulture);
+                var timeSpan = TimeSpan.FromSeconds(totalTime.TimeOfDay.TotalSeconds * vlcControl1.Position);
+                timeSpan = TimeSpan.FromSeconds(totalTime.TimeOfDay.TotalSeconds - timeSpan.TotalSeconds);
+                DateTime curTime = totalTime.Subtract(timeSpan);
+                lblCurRunTime.Text = String.Format("{0:HH:mm:ss} of {1:HH:mm:ss}", curTime, totalTime);
+            }
+            lblCurRunTime.Visible = v;
+            m_bShowRunTime = v;
+        }
+
+    private void refreshFiles()
+    {
+        lvMain.Items.Clear();
+        if (sCurDir != startDirectory)
+        {
+            ListViewItem lvi = lvMain.Items.Add("..");
+            lvi.ForeColor = Color.Green;
+            lvi.Tag = PATHCONSTANT;
+        }
+
+        // Directories
+        foreach( String sElement in Directory.GetDirectories( sCurDir ) )
+        {
+            String sCur = sElement.ToUpper().Substring(sElement.ToUpper().LastIndexOf('\\') + 1, sElement.Length - sElement.ToUpper().LastIndexOf('\\')-1);
+            ListViewItem lvi = lvMain.Items.Add( sCur );
+            lvi.ForeColor = Color.LightGreen;
+            lvi.Tag = PATHCONSTANT;
+        }
+    
+        foreach( String sElement in Directory.GetFiles( sCurDir, "*.mp4"  ) )
+        {
+            String sCur = sElement.Substring(sElement.ToUpper().LastIndexOf('\\') + 1, sElement.Length - sElement.ToUpper().LastIndexOf('\\')-1);
+            String[] sAllItems = new String[2];
+            sAllItems[0] = sCur;
+
+            dynamic shellApp = Activator.CreateInstance(Type.GetTypeFromProgID("Shell.Application"));
+            var folder = shellApp.NameSpace(Path.GetDirectoryName(sElement));
+            var file = folder.ParseName(Path.GetFileName(sElement));
+            string propertyValue = folder.GetDetailsOf(file, 27); // 27 represents a specific property, adjust as needed
+
+            sAllItems[1] = propertyValue;
+            ListViewItem lvi = new ListViewItem( sAllItems );
+            lvMain.Items.Add(lvi);
+            lvi.ForeColor = Color.White;
+                //lvi.Tag = PATHCONSTANT;
+        }
+
+        lvMain.Items[0].Selected = true;
+        lvMain.Focus();
+    }
+
+        private void tmrShowRuntime_Tick(object sender, EventArgs e)
+        {
+            if (m_bShowRunTime)
+            {
+                showRunTime(m_bShowRunTime);
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Delete &&
+                ((Control.ModifierKeys & Keys.Control) == Keys.Control))
+            {
+                if (lvMain.SelectedItems[0].Tag == null || lvMain.SelectedItems[0].Tag.ToString() != PATHCONSTANT)
+                {
+                    String sCurFile = sCurDir + '\\' + lvMain.SelectedItems[0].SubItems[0].Text;
+                    File.Delete(sCurFile);
+                    refreshFiles();
+                }
+            }
+            return;
+        }
+
+        private const string keyBase = @"SOFTWARE\Microsoft\Windows\CurrentVersion\App Paths";
+        private string GetPathForExe(string fileName)
+        {
+            RegistryKey localMachine = Registry.LocalMachine;
+            RegistryKey fileKey = localMachine.OpenSubKey(string.Format(@"{0}\{1}", keyBase, fileName));
+            object result = null;
+            if (fileKey != null)
+            {
+                result = fileKey.GetValue(string.Empty);
+                fileKey.Close();
+            }
+
+            return (string)result;
+        }
+    }
+
+    class IniFile   // revision 11
+    {
+        string Path;
+        string EXE = Assembly.GetExecutingAssembly().GetName().Name;
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        static extern long WritePrivateProfileString(string Section, string Key, string Value, string FilePath);
+
+        [DllImport("kernel32", CharSet = CharSet.Unicode)]
+        static extern int GetPrivateProfileString(string Section, string Key, string Default, StringBuilder RetVal, int Size, string FilePath);
+
+        public IniFile(string IniPath = null)
+        {
+            Path = new FileInfo(IniPath ?? EXE + ".ini").FullName;
+        }
+
+        public string Read(string Key, string Section = null)
+        {
+            StringBuilder RetVal = new StringBuilder(1000);
+            GetPrivateProfileString(Section ?? EXE, Key, "", RetVal, 1000, Path);
+            return RetVal.ToString();
+        }
+
+        public void Write(string Key, string Value, string Section = null)
+        {
+            WritePrivateProfileString(Section ?? EXE, Key, Value, Path);
+        }
+
+        public void DeleteKey(string Key, string Section = null)
+        {
+            Write(Key, null, Section ?? EXE);
+        }
+
+        public void DeleteSection(string Section = null)
+        {
+            Write(null, null, Section ?? EXE);
+        }
+
+        public bool KeyExists(string Key, string Section = null)
+        {
+            return Read(Key, Section).Length > 0;
+        }
+    }
+}
